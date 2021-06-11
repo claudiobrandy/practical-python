8c3ea2f9f5c573dab851e1b265de57f5462069a8
Claudio Brandy <claudio.brandy@gmail.com>
2021-06-10 23:50:40 -0300

    ticker pass

--- Work/ticker.py
+++ Work/ticker.py
@@ -15,12 +15,20 @@ def make_dicts(rows, headers):
     for row in rows:
         yield dict(zip(headers, row))
 
+def filter_symbols(rows, names):
+    for row in rows:
+        if row['name'] in names:
+            yield row
+
 def parse_stock_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows, [0, 1, 4])
     rows = convert_types(rows, [str, float, float])
     rows = make_dicts(rows, ['name', 'price', 'change'])
     return rows
+
+def ticker(portfile, logfile, fmt):
+    pass
 
 if __name__ == '__main__':
     lines = follow('Data/stocklog.csv')
